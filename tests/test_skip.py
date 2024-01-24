bf671bb3ddf34804c49f46b215cd0a44b7de5fb0
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-01-24 18:37:36 +0300

    fixed logical mistakes in skip comments

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -11,7 +11,7 @@ from selene import browser, have
 
 def test_github_desktop(browser_management):
     if browser_management == 'mobile':
-        pytest.skip('Not a mobile resolution')
+        pytest.skip('Not a desktop resolution')
     browser.open('https://github.com/')
     browser.element('.HeaderMenu-link--sign-in').click()
     browser.element('.auth-form-header').should(have.text('Sign in to GitHub'))
@@ -19,7 +19,7 @@ def test_github_desktop(browser_management):
 
 def test_github_mobile(browser_management):
     if browser_management == 'desktop':
-        pytest.skip('Not a desktop resolution')
+        pytest.skip('Not a mobile resolution')
     browser.open('https://github.com/')
     browser.element('.HeaderMenu-toggle-bar').click()
     browser.element('.HeaderMenu-link--sign-in').click()
